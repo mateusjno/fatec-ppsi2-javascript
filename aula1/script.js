18a82eb93936562d6689b5d15b1ab14b5235a77b
Mateus <mjnoliveira02@gmail.com>
2024-08-26 19:15:23 -0300

    Inserindo arquivos de aula

---
+++ aula1/script.js
@@ -0,0 +1,22 @@
+var salario = 1000.56;
+var nome = 'Mateus';
+var casado = true;
+var conhecimentos = ['PHP','JS'];
+var endereco = {
+    rua: 'Rua X',
+    numero: 12,
+    bairro: 'Centro',
+    cidade: 'Sao Roque'
+};
+
+
+console.log(nome);
+console.log(endereco.bairro);
+console.log(conhecimentos[1]);
+
+
+function somaValores(a, b){
+    return(a+b);
+}
+
+console.log(somaValores(4,6));
---
+++ aula1/scriptteste.js
@@ -0,0 +1,14 @@
+var nomeVariavel = 10;
+    let nomeVariavel2 = 'Mateus';
+
+    function somaValores(){
+        let valor1 = 2;
+        let valor2 = 3;
+        console.log(valor1 + valor2);
+    }
+
+    somaValores();
+
+    console.log('Minha Mensagem');
+    console.log(nomeVariavel);
+    console.log(nomeVariavel2);
