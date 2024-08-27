90320d5e4a136cf8c742ec1d65b8a212c5ad1116
Mateus <179388557+mateusol9812@users.noreply.github.com>
2024-08-27 11:55:24 -0300

    Adicionando arquivos da aula 3

--- aula3/script1.js
+++ aula3/script1.js
@@ -1,11 +1,39 @@
 var nome = document.getElementById("nome");
+var idade = document.getElementById("idade");
+var sexo = document.getElementById("sexo");
+var cpf = document.getElementById("cpf");
 
+var areaTexto = document.getElementById("resultado");
+
+var btnInserir = document.getElementById("btnInserir");
+
+btnInserir.addEventListener('click', valida);
 
 
 function valida(){
+    let msgErro = "";
 
-    document.getElementById('resultado').value = nome.value;
+    let nomeLimpo = nome.value.trim();
+    let qtdCaract = nomeLimpo.length;
 
+    if(qtdCaract < 4){
+        msgErro += "Nome inválido, necessário mais que 4 caracteres!\n";
+        nome.style.borderColor = 'red';
+    }
+
+    
+    if(idade.value < 21 && sexo.value == feminino){
+        msgErro += "Idade inválida";
+    }
+    else if (idade.value < 21 && sexo.value == feminino)
+
+
+
+
+
+
+    areaTexto.value = msgErro;
+    
 }
 
 
