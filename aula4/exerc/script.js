c5de5abe61142abe40bf2d74c4d66805062df364
Mateus Oliveira <179388557+mateusol9812@users.noreply.github.com>
2024-09-04 12:47:04 -0300

    Finalizado exercício Aula 4

--- aula4/exerc/script.js
+++ aula4/exerc/script.js
@@ -9,10 +9,14 @@ var btnInserir = document.getElementById("btnInserir");
 btnInserir.addEventListener('click', funcaoValidar);
 
 var alertaGeral;
+var contador = 0;
 
 function funcaoValidar(){
 
+    
     alertaGeral = false;
+    contador = 0;
+    
 
     adicionaNot(nome, " ");
     adicionaNot(tipo, " ");
@@ -20,22 +24,112 @@ function funcaoValidar(){
     adicionaNot(rgie, " ");
     adicionaNot(cep, " ");
 
-    let nomeLimpo = nome.value.trim();
+
+
+    /* Lógicas de decisão */
+
+        /* Nome */
+        let nomeTrim = nome.value.trim();
+        let nomeSemEspaco = nomeTrim.replace(" ", "");
+        const palavrasSeparadas = nomeTrim.split(" ");
+        let tamArray = palavrasSeparadas.length;
+
+        if (tamArray < 2 || tamArray > 2) {
+            alertaGeral = true;
+            adicionaNot(nome, "Necessário duas palavras no nome!");
+        }
+        else if(nomeSemEspaco.length < 4){
+            alertaGeral = true;
+            adicionaNot(nome, "Necessário ao menos 4 caracteres");
+        }
+        else {
+            alertaGeral = false;
+            adicionaNot(nome, "Nome válido!!!");
+        }
+        /* Nome */
+
+
+        /* CPF CNPJ */
+        let cpfcnpjSubst = cpfcnpj.value.replaceAll("/","").replaceAll(".","");
+        let cpfcnpjLimpo = cpfcnpjSubst.trim();
+
+        if(tipo.value == "pf" && (cpfcnpjLimpo.length < 11 || cpfcnpjLimpo.length > 11)){
+            alertaGeral = true;
+            adicionaNot(cpfcnpj, "CPF inválido!!!");
+        }
+        else if(tipo.value == "pj" && (cpfcnpjLimpo.length < 14 || cpfcnpjLimpo.length > 14)){
+            alertaGeral = true;
+            adicionaNot(cpfcnpj, "CNPJ inválido!!!");
+        }
+        else if(cpfcnpjLimpo.length == 0){
+            alertaGeral = true;
+            adicionaNot(cpfcnpj, "Inválido!!!");
+        }
+        else{
+            alertaGeral = false;
+            adicionaNot(cpfcnpj, "Válido!!!");
+        }
+
+        /* CPF CNPJ */
+
+
+
+        /* rg ie */
+        let rgieSubst = rgie.value.replaceAll("/","").replaceAll(".","");
+        let rgieLimpo = rgieSubst.trim();
 
     
-    if(nomeLimpo.length < 4 || nomeLimpo.length > 400){
-        alertaGeral = true;
-        adicionaNot(nome, "Nome inválido, necessário mais que 4 caracteres e menos que 400");
-    }
-    else{
-        adicionaNot(nome, "Nome válido!!");
-    }
+
+        if(tipo.value == "pf" && (rgieLimpo.length < 9 || rgieLimpo.length > 9)){
+            alertaGeral = true;
+            adicionaNot(rgie, "RG inválido!!!");
+        }
+        else if(tipo.value == "pj" && (rgieLimpo.length < 12 || rgieLimpo.length > 12)){
+            alertaGeral = true;
+            adicionaNot(rgie, "IE inválido!!!");
+        }
+        else if(rgieLimpo.length == 0){
+            alertaGeral = true;
+            adicionaNot(rgie, "Inválido!!!");
+        }
+        else{
+            alertaGeral = false;
+            adicionaNot(rgie, "Válido!!!");
+        }
+
+        /* rg ie */
+
+
+
+        /* cep */
+        let cepSubst = cep.value.replaceAll("/","").replaceAll(".","");
+        let cepLimpo = cepSubst.trim();
+
+
+        if(cepLimpo.length < 8 || cepLimpo.length > 8){
+            alertaGeral = true;
+            adicionaNot(cep, "CEP Inválido!!!");
+        }
+        else if(rgieLimpo.length == 0){
+            alertaGeral = true;
+            adicionaNot(cep, "CEP Inválido!!!");
+        }
+        else{
+            alertaGeral = false;
+            adicionaNot(cep, "Válido!!!");
+        }
+
+        /* cep */
+
+
+
 
 
 
 }
 
-
+    /* Função de notificação de alerta */
+        
 function adicionaNot (input, mensagem){
     let notge;
 
@@ -46,19 +140,20 @@ function adicionaNot (input, mensagem){
         p[0].textContent =  mensagem;
         p[0].style.color = "red";
         notge = document.getElementsByClassName("notificacao-geral")[0];
-        notge.textContent = "Formulário está com alguma problema observe as informações a baixo!!!";
+        notge.textContent = "Formulário está com algum problema observe as informações abaixo!!!";
         notge.style.color = "white";
         notge.classList.remove("hidden");
         notge.classList.add("visible");
         notge.classList.remove("estiloalertaverde");
         notge.classList.add("estiloalertavermelho");
+        contador++;
 
     }
     else{
         input.style.borderColor = "green";
         let p = document.getElementsByClassName("notificacao-"+input.id);
         p[0].textContent =  mensagem;
-        p[0].style.color = "green"
+        p[0].style.color = "green";
         notge = document.getElementsByClassName("notificacao-geral")[0];
         notge.textContent = "Formulário preenchido corretamente!!!";
         notge.style.color = "white";
@@ -67,7 +162,18 @@ function adicionaNot (input, mensagem){
         notge.classList.remove("estiloalertavermelho");
         notge.classList.add("estiloalertaverde");
 
-    } 
+    }
+
+
+    if (contador > 0){
+        notge = document.getElementsByClassName("notificacao-geral")[0];
+        notge.textContent = "Formulário está com algum problema observe as informações abaixo!!!";
+        notge.style.color = "white";
+        notge.classList.remove("hidden");
+        notge.classList.add("visible");
+        notge.classList.remove("estiloalertaverde");
+        notge.classList.add("estiloalertavermelho");
+    }
 }
 
 
